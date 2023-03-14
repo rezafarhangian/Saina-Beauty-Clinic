7d45c3d03e4fd048cc8e44a403800e0b641c25fd
Reza farhang <rezafarhangian79@gmail.com>
2023-03-14 18:44:03 +0330

    The slider is finished

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 
-export default function Slider() {
+export default function Slider({ name, education, Expertise, img }) {
   return (
-    <div>
-      <div className="bg-white relative p-2 w-60 m-auto h-44 rounded-xl shadow-[5px_16px_43px_28px_rgba(0,0,0,0.15)]">
-        <div className="mr-12">
+    <div className="relative pr-5 mx-auto h-60 lg:h-72 xl:h-80  flex items-center justify-center ">
+      <div className="bg-white absolute   p-2 w-60 sm:w-72 sm:p-4 h-44 sm:h-48 lg:w-80 lg:h-56 xl:w-[400px] xl:h-64 rounded-xl shadow-[rgba(99,99,99,0.2)_0px_2px_8px_0px]">
+        <div className="mr-16 sm:mr-20 lg:text-xl lg:mt-4 xl:text-2xl">
           <h3>
-            خانم دکتر <span className="text-Saina">مینا بنان</span>
+            خانم دکتر <span className="text-Saina">{name}</span>
           </h3>
           <h4 className="text-xs text-gray-500">پوست ، مو و زیبایی</h4>
         </div>
-          <ul className="list-disc text-xs mr-20 mt-5">
-            <li className="w-40">دارای مدرک استتیک (زیبایی) از بیوکر کانادا</li>
-            <li className="w-40">تزریق بوتاکس, تزریق پروفایلو, تزریق هیر فیلر</li>
-          </ul>
-          <img className="absolute top-12 w-28 -right-12  rounded-[30px]" src="./Images/Specialists/doctor1.png" alt="" />
+        <ul className="list-disc text-[#363E4E]  text-[10px] sm:text-xs mr-20 mt-5 lg:mt-9 xl:mr-28">
+          <li className="w-36 lg:w-44">{education}</li>
+          <li className="w-36 lg:w-44 mt-2">{Expertise}</li>
+        </ul>
+        <img
+          className="absolute -right-9 top-10  w-[105px] sm:w-[120px] sm:-right-16 lg:w-[140px] lg:-right-20 xl:w-[160px] xl:top-12 rounded-[30px] "
+          src={img}
+          alt=""
+        />
       </div>
     </div>
+
+
+
+   
   );
 }
--- src/Components/Specialists.jsx
+++ src/Components/Specialists.jsx
@@ -1,14 +1,39 @@
 import React from 'react'
 import Slider from './Slider'
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+
 
 export default function Specialists() {
   return (
-    <div className='mb-20'>
+    <div className='mb-20 relative '>
         <div className='text-center mb-7'>
             <h2 className='text-3xl md:text-5xl'>متخصصان <span className='text-Saina'>ساینا</span></h2>
         </div>
+         
+        <img className='absolute -top-20 right-[250px] xl:right-[370px] hidden lg:block' src="./Images/Specialists/Ellipse.svg" alt="" />
 
-        <Slider/>
+        <Swiper
+        loop={true}
+        pagination={{
+          dynamicBullets: true,
+
+        }}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        modules={[Pagination, Autoplay]}
+        className=""
+      > 
+        <SwiperSlide > <Slider name={"عاطفه رمضانی"} img={"./Images/Specialists/doctor1.jpg"} education={"دارای مدرک استتیک (زیبایی) از بیوکر کانادا"} Expertise={"تزریق بوتاکس, تزریق پروفایلو, تزریق هیر فیلر"}/></SwiperSlide>
+        <SwiperSlide > <Slider name={"زهرا زندیه"} img={"./Images/Specialists/doctor2.jpg"} education={"دانش اموخته دانشگاه علوم پزشکی تهران"} Expertise={"بوتاکس، درمان ریزش مو،RF فرکشنال"}/></SwiperSlide>
+        <SwiperSlide > <Slider name={"هاجر معمار"} img={"./Images/Specialists/doctor3.jpg"} education={"دارای بورد تخصصی از دانشگاه شهید بهشتی"} Expertise={" آر اف میداس, میکرونیدلینگ, هایفو, ونوس ویوا"}/></SwiperSlide>
+      </Swiper>
+        
+      
     </div>
   )
 }
