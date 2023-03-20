a3fadfae79df1e8463c7fe8fcc880b6413f62bed
Reza farhang <rezafarhangian79@gmail.com>
2023-03-20 23:01:58 +0330

    ui search box mobile size is handled

--- src/Components/NavbarMobile.jsx
+++ src/Components/NavbarMobile.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef  } from "react";
 import { BiSearchAlt } from "react-icons/bi";
 import SidebarMobile from "./SidebarMobile";
 
 export default function NavbarMobile() {
   const [showSidebar, setShowSidebar] = useState(false);
-
+  const [openSearchBox, setOpenSearchBox] = useState(false)
+  const inputRef = useRef();
 
   useEffect(() => {
     
@@ -15,14 +16,20 @@ export default function NavbarMobile() {
     }
 
   }, [showSidebar]);
-
+    
+ 
+ useEffect(() => {
+  inputRef.current.focus();
+})
+ 
   return (
     <div className="lg:hidden container shadow-[0_13px_36px_30px_rgba(83,20,153,0.06)] h-10 rounded-3xl	mt-10">
       <div className="flex items-center justify-between h-full">
         <div onClick={() => setShowSidebar(true)} className="cursor-pointer">
           <img src="./Images/IconNavBar/menu.png" alt="" />
         </div>
-        <div className="bg-Saina rounded-full w-8 h-8 flex items-center justify-center">
+        <div  onClick={() => setOpenSearchBox(true)}  className={`bg-Saina ${openSearchBox ? "w-48 rounded-2xl p-2" : "w-8 rounded-full p-0"}   h-8 flex items-center justify-center`}>
+           <input ref={inputRef}  onBlur={() => setOpenSearchBox(false)} className={`bg-transparent  ${openSearchBox ? "block" : "hidden"} w-full border-0 outline-0 text-xs placeholder:text-white placeholder:text-xs text-white`} type="text" placeholder="جست و جو"/>
           <BiSearchAlt className="text-xl cursor-pointer text-white" />
         </div>
       </div>
